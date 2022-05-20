1a78adddef0b29a5918c5518438ccc85d0eeedbf
Jan Trienes <jan.trienes@uni-due.de>
2022-05-20 14:40:25 +0200

    Update paths in training script

--- src/WGSUM_Trans.sh
+++ src/WGSUM_Trans.sh
@@ -1,19 +1,16 @@
+#!/bin/sh
+DATA_PATH=../data/mimic/bert/radiology
+MODEL_PATH=../models/mimic-replication
 
-Model_path=transformer_openi_model
-
-# gpus=4,5,6,7
-gpus=4
-log_file=$Model_path.log
-DATA_PATH=../bert_openI/radiology/radiology
-
+# Training
 python train.py \
 -mode train -accum_count 5 \
 -batch_size 300 \
 -bert_data_path $DATA_PATH \
 -dec_dropout 0.1 \
--log_file ../logs/$log_file \
+-log_file $MODEL_PATH/training.log \
 -lr 0.05 \
--model_path $Model_path \
+-MODEL_PATH $MODEL_PATH \
 -save_checkpoint_steps 200 \
 -seed 777 \
 -sep_optim false \
@@ -21,7 +18,7 @@ python train.py \
 -use_bert_emb true \
 -use_interval true \
 -warmup_steps 8000  \
--visible_gpus $gpus \
+-visible_gpus 1,2,3,4,5 \
 -max_pos 512 \
 -report_every 50 \
 -enc_hidden_size 512  \
@@ -35,27 +32,21 @@ python train.py \
 -task abs
 
 
-
-Model_path=transformer_openi_model
-echo $Model_path
-log_file2=$Model_path.testlog
-result_file=$Model_path.result
-gpus=1
+# Validate
 python train.py \
 -task abs \
 -mode validate \
 -batch_size 3000 \
 -test_batch_size 500 \
--bert_data_path DATA_PATH \
--log_file ../logs/$log_file2 \
--model_path $Model_path \
+-bert_data_path $DATA_PATH \
+-log_file $MODEL_PATH/validate.log \
+-MODEL_PATH $MODEL_PATH \
 -sep_optim true \
 -use_interval true \
--visible_gpus $gpus \
+-visible_gpus 0 \
 -max_pos 512 \
 -max_length 50 \
 -alpha 0.95 \
 -min_length 6 \
--result_path ../logs/$result_file \
+-result_path $MODEL_PATH/summaries \
 -test_all
-
