33c4b7b18a2d19684549ee5563b8118383e57f6d
Jan Trienes <jan.trienes@uni-due.de>
2022-05-20 14:47:36 +0200

    Split training and validation scripts

--- src/WGSUM_train.sh
+++ src/WGSUM_train.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 DATA_PATH=../data/mimic/bert/radiology
 MODEL_PATH=../models/mimic-replication
+GPUS=1,2,3,4,5
 
 # Training
 python train.py \
@@ -18,7 +19,7 @@ python train.py \
 -use_bert_emb true \
 -use_interval true \
 -warmup_steps 8000  \
--visible_gpus 1,2,3,4,5 \
+-visible_gpus $GPUS \
 -max_pos 512 \
 -report_every 50 \
 -enc_hidden_size 512  \
@@ -30,23 +31,3 @@ python train.py \
 -dec_ff_size 2048 \
 -encoder baseline \
 -task abs
-
-
-# Validate
-python train.py \
--task abs \
--mode validate \
--batch_size 3000 \
--test_batch_size 500 \
--bert_data_path $DATA_PATH \
--log_file $MODEL_PATH/validate.log \
--model_path $MODEL_PATH \
--sep_optim true \
--use_interval true \
--visible_gpus 0 \
--max_pos 512 \
--max_length 50 \
--alpha 0.95 \
--min_length 6 \
--result_path $MODEL_PATH/summaries \
--test_all
